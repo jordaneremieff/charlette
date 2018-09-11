f30c2f0ec5a42d75d9e8d26dadcabefd9b574853
erm <jermff@gmail.com>
2018-09-12 02:15:21 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     packages=find_packages(),
     install_requires=["Django", "channels", "starlette", "innate"],
     license="MIT License",
+    url="https://github.com/erm/charlette",
+    description="Django app that uses Starlette components with Channels.",
     entry_points={"console_scripts": ["charlette = charlette.__main__:innate.cli"]},
     author="Jordan Eremieff",
     author_email="jordan@eremieff.com",
